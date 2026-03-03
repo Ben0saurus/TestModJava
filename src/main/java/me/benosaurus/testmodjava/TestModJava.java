f3876709a5c7ebe871523c2152e2512a490149fd
Ben0saurus <ben.kremer+gh@outlook.de>
2026-03-03 19:08:55 +0100

    Initial commit

---
+++ src/main/java/me/benosaurus/testmodjava/TestModJava.java
@@ -0,0 +1,15 @@
+package me.benosaurus.testmodjava;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestModJava implements ModInitializer {
+	public static final String MOD_ID = "testmodjava";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/me/benosaurus/testmodjava/TestModJavaClient.java
@@ -0,0 +1,10 @@
+package me.benosaurus.testmodjava;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestModJavaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
