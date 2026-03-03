da4d8427ff9a01f228a4c0a8304157a8822616e6
Ben0saurus <ben.kremer+gh@outlook.de>
2026-03-03 20:43:16 +0100

    added tekno block

--- src/main/java/me/benosaurus/testmodjava/TestModJava.java
+++ src/main/java/me/benosaurus/testmodjava/TestModJava.java
@@ -1,5 +1,6 @@
 package me.benosaurus.testmodjava;
 
+import me.benosaurus.testmodjava.block.ModBlocks;
 import me.benosaurus.testmodjava.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
@@ -13,5 +14,6 @@ public class TestModJava implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
--- src/main/java/me/benosaurus/testmodjava/block/ModBlocks.java
+++ src/main/java/me/benosaurus/testmodjava/block/ModBlocks.java
@@ -1,4 +1,47 @@
 package me.benosaurus.testmodjava.block;
 
+import me.benosaurus.testmodjava.TestModJava;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+
 public class ModBlocks {
+
+    public static final Block TEKNO_BLOCK = registerBlock("tekno_block",
+            new Block(AbstractBlock.Settings.create()
+                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(TestModJava.MOD_ID, "tekno_block")))
+                    .strength(2f)
+                    .requiresTool()
+                    .sounds(BlockSoundGroup.MUD)
+                    .slipperiness(4f)));
+
+    private static Block registerBlock(String name, Block block) {
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, Identifier.of(TestModJava.MOD_ID, name), block);
+    }
+
+    private static void registerBlockItem(String name, Block block) {
+        Registry.register(Registries.ITEM, Identifier.of(TestModJava.MOD_ID, name),
+                new BlockItem(block, new Item.Settings()
+                        .registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(TestModJava.MOD_ID, name)))
+                        .useBlockPrefixedTranslationKey()));
+    }
+
+    public static void registerModBlocks() {
+        TestModJava.LOGGER.info("Registering Mod Blocks for " + TestModJava.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
+            entries.add(TEKNO_BLOCK);
+        });
+    }
+
 }
